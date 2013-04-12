eb4b65615c7dfccb1fa7055513c0ca1960a3fb39
Chris Risner <github@chrisrisner.com>
2013-04-12 10:50:49 -0700

    Clean up

--- source/client/AuthenticationDemo/src/com/msdpe/authenticationdemo/AuthService.java
+++ source/client/AuthenticationDemo/src/com/msdpe/authenticationdemo/AuthService.java
@@ -1,3 +1,19 @@
+/*
+ Copyright 2013 Microsoft Corp
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package com.msdpe.authenticationdemo;
 
 import java.net.MalformedURLException;
@@ -46,8 +62,8 @@ public class AuthService {
 		public AuthService(Context context) {
 			mContext = context;
 			try {
-				mClient = new MobileServiceClient("https://myauthdemo.azure-mobile.net/", 
-						"HZatbbcDTUXflXkUFIlkcqeFxPMppl54", mContext)
+				mClient = new MobileServiceClient("https://MyMobileServiceName.azure-mobile.net/", 
+						"MyMobileServiceApplicationKey", mContext)
 						.withFilter(new MyServiceFilter());
 				mTableAccounts = mClient.getTable("Accounts");
 				mTableAuthData = mClient.getTable("AuthData");
